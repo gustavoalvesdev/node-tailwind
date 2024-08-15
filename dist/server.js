216ff85ee0292f9a80647d8804e6614e995f47bc
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-08-15 17:23:17 -0300

    exemplo node tailwind

---
+++ dist/server.js
@@ -0,0 +1,4 @@
+"use strict";
+const nome = 'Gustavo';
+const idade = 32;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+const nome: string = 'Gustavo';
+const idade: number = 32;
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);
